cae09e2520a6d83098994d81a0a0c19d299f3c9c
odieste <odieste@fi.upm.es>
2024-11-06 19:28:01 +0100

    make the build fail

---
+++ src/test/java/es/upm/grise/profundizacion/mavenExample/AppIT.java
@@ -0,0 +1,15 @@
+package es.upm.grise.profundizacion.mavenExample;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppIT {
+
+	@Test
+	public void test() {
+		
+		fail();
+		
+	}
+
+}
